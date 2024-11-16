3dae766c7494a9c9617f5fea6227273b7003761d
MaxKolisnichenko <126777664+MaxKolisnichenko@users.noreply.github.com>
2024-11-16 17:24:59 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+const quoteContainer = document.querySelector('.quote_container');
+const quoteText = document.querySelector('.quote');
+const newQuoteBtn = document.getElementById('new_quote');
+
+const getQuote = async () => {
+  const apiUrl = 'https://api.quotable.io/random';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    quoteText.textContent = data.content;
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+  }
+};
+
+newQuoteBtn.addEventListener('click', getQuote);
+
+getQuote();
+
